a90f411670f133e53e95d387a912bbda399b638b
Timothy Hoang <timh1203@gmail.com>
2018-06-06 12:51:23 -0400

    Style bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,22 +6,24 @@ import FaInstagram from 'react-icons/lib/fa/instagram'
 import { Section, Div, Img, H1, P, A } from '../assets/Matrix'
 
 const Footer = () => (
-  <Section id='Bio'>
-    <H1>Bio Section</H1>
+  <Section id='Bio' modifiers={['vh50']}>
     <Div modifiers={['w50', 'm0A', 'tCenter']}>
+      <H1>Whatever good things we build end up building us.</H1>
+      <P>-Jim Rohn</P>
       <Img modifiers={['bRadius50', 'w25', 'borderBlack']} src='https://i.imgur.com/vxJq5Wl.jpg' />
       <P>
         I am an architectural designer with a strong passion for sustainability and innovation. My goal in Architecture and photography is to reveal the beauty in ordinary things. To show the detail and the unique aspects of life that may be missed when one isn't paying attention. To draw people in to not just see the world, but to experience it through my designs. To inspire others to go outside the box, to live, to explore, and to adventure.
       </P>
       
-      <Div modifiers={['tCenter']}>
+      <Div>
         <A href='https://www.linkedin.com/in/derik-jones-b3722b102/'>
           <FaLinkedinSquare 
             style={{
               color: '#0077B5',
               border: '1px solid #0077B5',
               borderRadius: '10%',
-              boxShadow: '0px 3px 10px rgba(0,0,0,.25)'
+              boxShadow: '0px 3px 10px rgba(0,0,0,.25)',
+              margin: '2rem',
             }} 
             size={50} 
           />
@@ -33,7 +35,8 @@ const Footer = () => (
               borderRadius: '10%',
               background: '#d6249f',
               background: 'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)',
-              boxShadow: '0px 3px 10px rgba(0,0,0,.25)'
+              boxShadow: '0px 3px 10px rgba(0,0,0,.25)',
+              margin: '2rem',
             }}
             size={50}
           />
