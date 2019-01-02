0ed199ed5533556d14c22446a90f67ce4af38207
Timothy Hoang <timh1203@gmail.com>
2019-01-02 12:11:30 -0600

    Fix design and projects

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -1,37 +1,63 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import { Div, H1, P, P1, Img1 } from '../assets/Matrix'
+import { Div, P } from '../assets/Matrix'
 
 const Designs = () => (
-  <Div modifier={['py1']}>
+  <Div0>
     <P1>3d Designs</P1>
     <Div1>
-      <Div modifiers={['pt3']}>
+      <Div2>
         <Link to="/design1">
+          <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 1" />
+        </Link>
+        <H1a>Conceptual Design</H1a>
+        <P2>Sketchup & V-ray</P2>
+      </Div2>
+      <Div2>
+        <Link to="/design2">
+          <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 2" />
+        </Link>
+        <H1a>Conceptual Design</H1a>
+        <P2>Sketchup & V-ray</P2>
+      </Div2>
+      <Div2>
+        <Link to="/design3">
+          <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 3" />
+        </Link>
+        <H1a>Conceptual Design</H1a>
+        <P2>Sketchup & V-ray</P2>
+      </Div2>
+      <Div2>
+        <Link to="/design4">
           <Img1 src="https://i.imgur.com/7USkFg2.png" alt="design 1" />
         </Link>
-        <H1>Teagarden</H1>
-        <P>Illustrator Design & Photoshop</P>
-      </Div>
-      <Div modifiers={['pt3']}>
-        <Link to="/design2">
+        <H1a>Teagarden</H1a>
+        <P2>Illustrator Design & Photoshop</P2>
+      </Div2>
+      <Div2>
+        <Link to="/design5">
           <Img1 src="https://i.imgur.com/yGY60ZS.png" alt="design 2" />
         </Link>
-        <H1>Grand Villas-Hudson</H1>
-        <P>Shetckup Design</P>
-      </Div>
-      <Div modifiers={['pt3']}>
-        <Link to="/design3">
+        <H1a>Grand Villas-Hudson</H1a>
+        <P2>Shetckup Design</P2>
+      </Div2>
+      <Div2>
+        <Link to="/design6">
           <Img1 src="https://i.imgur.com/idFt6go.png" alt="design 3" />
         </Link>
-        <H1>Middleway</H1>
-        <P>Illustrator Design & Photoshop</P>
-      </Div>
+        <H1a>Middleway</H1a>
+        <P2>Illustrator Design & Photoshop</P2>
+      </Div2>
     </Div1>
-  </Div>
+  </Div0>
 )
 
+const Div0 = styled.div`
+  padding: 1.5rem 0;
+  width: 90%;
+  margin: 0 auto;
+`
 const Div1 = styled(Div)`
   display: grid;
   text-align: center;
@@ -40,6 +66,51 @@ const Div1 = styled(Div)`
 
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
+  }
+`
+const Div2 = styled.div`
+  padding-top: 3rem 0;
+`
+const P1 = styled.p`
+  font-family: 'Crimson Text', serif;
+  font-size: 1.6rem;
+  padding: 0.4rem 0;
+  text-align: center;
+  text-decoration: underline;
+  transform: rotate(-5deg);
+  border: 1px solid black;
+  margin: 0 auto;
+  margin-bottom: 4rem;
+  background: #333333;
+  color: white;
+  border-radius: 5px;
+  width: 10%;
+  @media (max-width: 768px) {
+    width: 18%;
+    font-size: 1.2rem;
+  }
+  @media (max-width: 376px) {
+    width: 40%;
+  }
+`
+const H1a = styled.h1`
+  font-family: 'Crimson Text', serif;
+  margin-bottom: 2rem;
+  `
+const P2 = styled.p`
+  font-family: 'Crimson Text', serif;
+  font-size: 1.6rem;
+  margin-bottom: 2rem;
+`
+const Img1 = styled.img`
+  width: 75%;
+  margin-bottom: 2rem;
+  &:hover {
+    filter: brightness(80%);
+    box-shadow: 0 0 10px #82DBFA;
+  }
+  @media (max-width: 376px) {
+    width: 90%;
   }
 `
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,58 +1,42 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import { Div, H1, P, P1, Img1 } from '../assets/Matrix'
+import { Div } from '../assets/Matrix'
 
 const Projects = () => (
-  <Div modifier={['py1']}>
+  <Div0>
     <P1>Projects</P1>
     <Div1>
-      <Div modifiers={['pt3']}>
-        <Link to="/project1">
-          <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 1" />
-        </Link>
-        <H1>Conceptual Design</H1>
-        <P>Sketchup & V-ray</P>
-      </Div>
-      <Div modifiers={['pt3']}>
-        <Link to="/project1">
-          <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 2" />
-        </Link>
-        <H1>Conceptual Design</H1>
-        <P>Sketchup & V-ray</P>
-      </Div>
-      <Div modifiers={['pt3']}>
-        <Link to="/project1">
-          <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 3" />
-        </Link>
-        <H1>Conceptual Design</H1>
-        <P>Sketchup & V-ray</P>
-      </Div>
-      <Div modifiers={['pt3']}>
+      <Div2>
         <Link to="/project1">
           <Img1 src="https://i.imgur.com/k0dZG6j.png" alt="Project 4" />
         </Link>
-        <H1>Brambleton Virginia</H1>
-        <P>Multi-Family</P>
-      </Div>
-      <Div modifiers={['pt3']}>
+        <H1a>Brambleton Virginia</H1a>
+        <P2>Multi-Family</P2>
+      </Div2>
+      <Div2>
         <Link to="/project2">
           <Img1 src="https://i.imgur.com/yAUDSX7.png" alt="Project 5" />
         </Link>
-        <H1>Brambleton Virginia</H1>
-        <P>Townhome</P>
-      </Div>
-      <Div modifiers={['pt3']}>
+        <H1a>Brambleton Virginia</H1a>
+        <P2>Townhome</P2>
+      </Div2>
+      <Div2>
         <Link to="/project3">
           <Img1 src="https://i.imgur.com/qXkvDah.png" alt="Project 6" />
         </Link>
-        <H1>Leesburg South</H1>
-        <P>Single Family</P>
-      </Div>
+        <H1a>Leesburg South</H1a>
+        <P2>Single Family</P2>
+      </Div2>
     </Div1>
-  </Div>
+  </Div0>
 )
 
+const Div0 = styled.div`
+  padding: 1.5rem 0;
+  width: 90%;
+  margin: 0 auto;
+`
 const Div1 = styled(Div)`
   display: grid;
   text-align: center;
@@ -63,7 +47,51 @@ const Div1 = styled(Div)`
     grid-template-columns: 1fr;
   }
 `
-
+const Div2 = styled.div`
+  padding-top: 3rem 0;
+`
+const P1 = styled.p`
+  font-family: 'Crimson Text', serif;
+  font-size: 1.6rem;
+  padding: 0.4rem 0;
+  text-align: center;
+  text-decoration: underline;
+  transform: rotate(-5deg);
+  border: 1px solid black;
+  margin: 0 auto;
+  margin-bottom: 4rem;
+  background: #333333;
+  color: white;
+  border-radius: 5px;
+  width: 10%;
+  @media (max-width: 768px) {
+    width: 18%;
+    font-size: 1.2rem;
+  }
+  @media (max-width: 376px) {
+    width: 40%;
+  }
+`
+const H1a = styled.h1`
+  font-family: 'Crimson Text', serif;
+  margin-bottom: 2rem;
+  `
+const P2 = styled.p`
+  font-family: 'Crimson Text', serif;
+  font-size: 1.6rem;
+  margin-bottom: 2rem;
+`
+const Img1 = styled.img`
+  width: 75%;
+  margin-bottom: 2rem;
+  &:hover {
+    filter: brightness(80%);
+    box-shadow: 0 0 10px #82DBFA;
+  }
+  @media (max-width: 376px) {
+    width: 90%;
+  }
+`
 // const Img1 = styled(Img)`
 //   &:hover {
 //     filter: brightness(80%);
